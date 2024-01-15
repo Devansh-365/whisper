1f8c5dfaa556ccee13d6bf412bfc0b0b748fb3fc
Devansh-365 <devanshtiwari365@gmail.com>
2024-01-16 00:05:33 +0530

    fix(client): backend url

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,4 +1,4 @@
-let BASE_URL = "https://ceddit.onrender.com/";
+let BASE_URL = "https://whisper-95lw.onrender.com";
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
   BASE_URL = "http://localhost:8080";
 }
